6ef88e48178e2806b7707e6d427afa9765eaac1a
mtthomas166 <mtthomas166@gmail.com>
2026-09-23 01:50:31 +0300

    Remove unused import for flutter/services.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
